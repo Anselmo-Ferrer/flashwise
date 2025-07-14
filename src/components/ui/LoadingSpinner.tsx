c4753445f9e61e2d8ae1555ee13fb6993c620ccf
Anselmo Dev <anselmoferrer.dev@gmail.com>
2025-07-13 23:39:10 -0300

    feat: add new ui components from shadcn ui

--- src/components/ui/LoadingSpinner.tsx
+++ src/components/ui/LoadingSpinner.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-export default function LoadingSpinner() {
+type LoadingSpinnerProps = {
+  width?: number
+  height?: number
+}
+
+export default function LoadingSpinner({ width, height }: LoadingSpinnerProps) {
   return (
-    <div className="flex items-center justify-center min-h-[100px]">
-      <div className="animate-spin rounded-full h-16 w-16 border-2 border-[#FFA500] border-t-transparent"></div>
+    <div className="flex items-center justify-center">
+      <div className={`animate-spin rounded-full h-[${height}px] w-[${width}px] border-2 border-[#FFA500] border-t-transparent`}></div>
       <span className="sr-only">Carregando...</span>
     </div>
   );
